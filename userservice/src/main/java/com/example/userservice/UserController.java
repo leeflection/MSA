aa499dcdb19daad745ae5c552965de25dfa2b154
leeflection <leeflection@gmail.com>
2023-05-15 17:19:49 +0900

    로드 밸런싱 부분까지 구현

--- userservice/src/main/java/com/example/userservice/UserController.java
+++ userservice/src/main/java/com/example/userservice/UserController.java
@@ -1,13 +1,20 @@
 package com.example.userservice;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController("/")
 @Slf4j
+@RequiredArgsConstructor
 public class UserController {
+
 
     @GetMapping("first-service")
     public String first(){
@@ -29,5 +36,21 @@ public class UserController {
     public String secondMessage(@RequestHeader("second-request") String header){
         log.info(header);
         return "second-service";
+    }
+
+    @GetMapping("second-service/check")
+    public String checkSecond(){
+        return "Hi, there, second check";
+    }
+
+//    @GetMapping("first-service/check")
+//    public String checkFirst(){
+//        return "Hi, there, first check";
+//    }
+    private final Environment env;
+    @GetMapping("first-service/check")
+    public String check(HttpServletRequest request){
+        log.info("Server port={}",request.getServerPort());
+        return String.format("HI Port %s",env.getProperty("local.server.port"));
     }
 }
